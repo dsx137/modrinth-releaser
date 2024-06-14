639ac7c3dabe21f336fed26373cd6f00bdafc9bf
dsx137 <1419696375@qq.com>
2024-06-14 13:11:20 +0800

    fix

--- dist/index.js
+++ dist/index.js
@@ -42859,14 +42859,15 @@ async function getFilesData() {
   });
 }
 
-const dependencies = _values_js__WEBPACK_IMPORTED_MODULE_5__/* .dependencies.split */ .HO.split(',')
+const dependencies = [];
+_values_js__WEBPACK_IMPORTED_MODULE_5__/* .dependencies.split */ .HO.split(',')
   .filter((dependency) => { return dependency != null && dependency != '' && dependency != undefined })
   .map((dependency) => {
     const [project_id, dependency_type] = dependency.split(':').map(it => it.trim());
     if (!['required', 'optional', 'incompatible', 'embedded'].includes(dependency_type)) {
       terminate(`Invalid dependency type: ${dependency_type}`)
     }
-    return { project_id, dependency_type }
+    dependencies.append({ project_id, dependency_type });
   });
 
 const game_versions = [];
